52e5e5290d575d5696f94b30d8874bf5de54068a
Kabilansgithub <kabilans004@gmail.com>
2025-03-22 13:11:13 +0530

    added header

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ui from '@nuxt/ui/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ui(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
